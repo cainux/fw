f2ce9e3d56bb824b9e7b258768172562f7d1fa78
Cain Voong <cainux@gmail.com>
2019-11-01 01:33:43 +0000

    Correctly testing for exceptions

--- tests/Movies.Tests/Services/Given_a_MovieService/When_fetching_Top_N_Movies_for_an_Invalid_User.cs
+++ tests/Movies.Tests/Services/Given_a_MovieService/When_fetching_Top_N_Movies_for_an_Invalid_User.cs
@@ -7,28 +7,18 @@ namespace Movies.Tests.Services.Given_a_MovieService
 {
     public class When_fetching_Top_N_Movies_for_an_Invalid_User : Given_a_MovieService
     {
-        private readonly UserNotFoundException actual = null;
+        private readonly Exception actual;
 
         public When_fetching_Top_N_Movies_for_an_Invalid_User()
         {
-            // Arrange
-            var userId = 1;
-
             // Act
-            try
-            {
-                _ = SUT.TopNMoviesAsync(userId, 5).Result;
-            }
-            catch (AggregateException e) // This is a bit fishy
-            {
-                actual = (UserNotFoundException) e.InnerException;
-            }
+            actual = Record.Exception(SUT.TopNMoviesAsync(1, 5).Wait);
         }
 
         [Fact]
         public void Then_UserNotFoundException_should_be_thrown()
         {
-            actual.Should().NotBeNull();
+            actual.InnerException.Should().BeOfType<UserNotFoundException>();
         }
     }
 }
--- tests/Movies.Tests/Services/Given_a_MovieService/When_searching_without_any_criteria.cs
+++ tests/Movies.Tests/Services/Given_a_MovieService/When_searching_without_any_criteria.cs
@@ -7,25 +7,18 @@ namespace Movies.Tests.Services.Given_a_MovieService
 {
     public class When_searching_without_any_criteria : Given_a_MovieService
     {
-        private readonly EmptySearchCriteriaException actual = null;
+        private readonly Exception actual;
 
         public When_searching_without_any_criteria()
         {
             // Act
-            try
-            {
-                _ = SUT.SearchMoviesAsync(null, null, null).Result;
-            }
-            catch (AggregateException e) // This is a bit fishy
-            {
-                actual = (EmptySearchCriteriaException) e.InnerException;
-            }
+            actual = Record.Exception(SUT.SearchMoviesAsync(null, null, null).Wait);
         }
 
         [Fact]
         public void Then_EmptySearchCriteriaException_should_be_thrown()
         {
-            actual.Should().NotBeNull();
+            actual.InnerException.Should().BeOfType<EmptySearchCriteriaException>();
         }
     }
 }
